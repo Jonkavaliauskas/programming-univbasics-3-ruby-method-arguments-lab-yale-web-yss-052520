ae951a8123bb044ddbef632d3e6b10f9fdb0c24e
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-12 15:29:30 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,10 @@ end
 introduction
 
 def introduction_with_language(name, language)
-  puts ""
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language
 
 
 
